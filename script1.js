d364ecffb904a4f1033816f9796950554b3732af
saikrishna-ganji <70600507+saikrishna-ganji@users.noreply.github.com>
2021-01-03 21:40:42 +0530

    display country timezones and capitals

--- script1.js
+++ script1.js
@@ -30,12 +30,40 @@ const main = async () => {
     countryFlag.className = 'country-flag'
     countryFlag.src = country.flag;
 
+
+    // create div to display country-capital <p class="country-capital">text</p>
+    const countryCapital = document.createElement('p');
+    countryCapital.className = 'country-capital'
+    // <span class="country-capital-title">text</span>
+    // const countryCapitalTitle = document.createElement('span')
+    // countryCapitalTitle.className = 'country-capital-title'
+    // countryCapitalTitle.innerHTML = 'Country Capital City: '
+
+    // using appencChild & append
+    // countryCapital.appendChild(countryCapitalTitle)
+    // countryCapital.append(country.capital)
+
+    // countryCapital.innerHTML = countryCapitalTitle.concat(country.capital);
+    // countryCapital.innerHTML = 'Country Capital City: '.concat(country.capital);
+    // countryCapital.innerHTML = 'Country Capital City: ' + country.capital;
+    countryCapital.innerHTML = `<span class="country-capital-title">Country Capital City </span> ${country.capital}`;
+
+
+    //create div to display country-timezone
+    const countryTimeZones = document.createElement('div')
+    countryTimeZones.className = 'country-timezones'
+    // countryTimeZones.innerHTML = country.timezones[0];
+    countryTimeZones.innerHTML = `<span class="country-timezone-title">Country Timezone :</span> ${country.timezones[0]}`; // Template-String syntax
+
+
     // ToDo: create div/span/p/h1/h2/h3/h4/h5 to display country-capital
 
     // ToDo: create div/span/p/h1/h2/h3/h4/h5 to display country-timezone
 
     countryCard.appendChild(countryName)
     countryCard.appendChild(countryFlag)
+    countryCard.appendChild(countryCapital)
+    countryCard.appendChild(countryTimeZones)
     
     rootDOMElement.appendChild(countryCard)
 
