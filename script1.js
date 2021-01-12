a25361e3fe7d2bc1282bbb592a93de4e31000cd1
Saritha-Kybing <saritha@kybing.com>
2021-01-12 23:39:51 +0530

    Added pagination functionality

--- script1.js
+++ script1.js
@@ -2,20 +2,79 @@
  * Links
  *  https://wiki.openstreetmap.org/wiki/OpenLayers_Simple_Example
  *  https://restcountries.eu/rest/v2/region/asia
+ *  https://restcountries.eu/rest/v2/all
  */
 const main = async () => {
   const API_KEY = 'AIzaSyAq3WKVwUzBZovzuiCo-VsP1bX1uih6Dag';
+  const ITEMS_PER_PAGE = 10;
+  let countriesList = [];
 
   // xml-http request to a web-server to fetch data, here web-server name is restcountries.eu
   const fetchCountriesList = async () => {
-    // fetch('https://restcountries.eu/rest/v2/all')
-    const response = await fetch('https://restcountries.eu/rest/v2/region/asia')
+    const response = await fetch('https://restcountries.eu/rest/v2/all')
     const data = await response.json()
     
     return data;
   }
-  const countriesList = await fetchCountriesList();
+  // countriesList = await fetchCountriesList();
   console.log('countriesList -> ', countriesList);
+
+  const fetchCountriesListWithLessData = async () => {
+    const response = await fetch('https://restcountries.eu/rest/v2/all?fields=alpha3Code');
+    const data = await response.json();
+   
+    return data;
+  }
+
+  const countriesListWithLessData = await fetchCountriesListWithLessData();
+  const countryCodesList = countriesListWithLessData.map((val) => {
+    return val.alpha3Code;
+  })
+  console.log('countryCodesList -> ', countryCodesList);
+
+  const fetchCountriesByCodes = async (codes) => {
+    const codesString = codes.join(';');
+    console.log('codes -> ', codes);
+    console.log('codesString -> ', codesString);
+    const response = await fetch(`https://restcountries.eu/rest/v2/alpha?codes=${codesString}`);
+    const data = await response.json();
+
+    return data;
+  }
+
+  const renderPagination = () => {
+    const paginationDOMElement = document.getElementById('pagination');
+    
+    for (let i = 0; i < countriesListWithLessData.length/ITEMS_PER_PAGE; i++) {
+      const pageNumber = i + 1;
+      const paginationButton = document.createElement('button');
+      paginationButton.className = 'pagination-button';
+      paginationButton.id = `pagination-button${i}`;
+      paginationButton.innerHTML = pageNumber;
+
+      paginationDOMElement.appendChild(paginationButton);
+
+      paginationButton.addEventListener('click', async () => {
+        const startIndex = ITEMS_PER_PAGE * i;
+        const endIndex = startIndex + ITEMS_PER_PAGE;
+
+        
+        // ToDo: Find method to add class-name to all elements at once instead of using for-loop
+        const previouslySelectedButton = document.querySelector('.pagination-button.select');
+        if (previouslySelectedButton) {
+          previouslySelectedButton.classList.remove('select');
+        }
+        
+        paginationButton.classList.add('select');
+
+        console.log('page ->', pageNumber, 'startIndex -> ', startIndex, 'endIndex -> ', endIndex);
+        const countriesListByCodes = await fetchCountriesByCodes(countryCodesList.slice(startIndex, endIndex));
+
+        clearCountries();
+        renderCountries(countriesListByCodes);
+      })
+    }
+  }
 
   const clearCountries = () => {
     const countriesDOMElement = document.getElementById('countries');
@@ -82,7 +141,7 @@ const main = async () => {
       // create div to display location (lat-lng)
       const countryLocation = document.createElement('div')
       countryLocation.className = 'country-location'
-      countryLocation.innerHTML = `<span class="country-location-title">Country Location : </span>${country.latlng[0].toFixed(2)}, ${country.latlng[1].toFixed(2)}`;
+      countryLocation.innerHTML = `<span class="country-location-title">Country Location : </span>${country.latlng[0] ? country.latlng[0].toFixed(2) : ''}, ${country.latlng[1] ? country.latlng[1].toFixed(2) : ''}`;
   
       // append to country-card.
       countryCard.appendChild(countryName)
@@ -166,7 +225,10 @@ const main = async () => {
       const searchText = searchField.value.toLowerCase();
 
       const filteredCountriesList = countriesList.filter((country) => {
-        return country.name.toLowerCase().includes(searchText)
+        if (country.name.toLowerCase().includes(searchText) || country.capital.toLowerCase().includes(searchText)) {
+          return true;
+        }
+        // return country.name.toLowerCase().includes(searchText) || country.capital.toLowerCase().includes(searchText);
       });
 
       clearCountries();
@@ -187,6 +249,7 @@ const main = async () => {
   }
 
   clearCountries();
+  renderPagination();
   renderCountries(countriesList);
   // searchCountriesUglyApproach();
   // clearSearchCountriesUglyApproach();
